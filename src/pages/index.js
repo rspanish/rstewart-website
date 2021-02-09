1c8036b8ba8ed60bdb7b2aad5574c97acc8f1051
rspanish <78628230+rspanish@users.noreply.github.com>
2021-02-08 23:48:42 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Index = () => {
+  return (
+    <Layout>
+      <h1>Home page</h1>
+      <h2>Robert Stewart</h2>
+    </Layout>
+  )
 }
+
+export default Index
