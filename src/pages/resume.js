56e342423688393dd08ec961ac19201792772417
rspanish <78628230+rspanish@users.noreply.github.com>
2021-02-08 23:48:44 -0500

    Create resume.js

---
+++ src/pages/resume.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Resume = () => {
+  return (
+    <layout>
+      <h1>Resume Page</h1>
+      <p>My resume will be displayed here!</p>
+      </layout>
+  )
+}
+
+export default Resume
